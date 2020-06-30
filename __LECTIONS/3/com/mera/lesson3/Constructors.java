8b2e2d768c3c05aaa26a360d8cae6399f0e42488
andreykarabash <66316656+andreykarabash@users.noreply.github.com>
2020-06-30 15:04:28 +0300

    merge2_3o (#2)
    * Lection #2 samples
    
    * 2nd homework
    
    * indentation
    
    * Task#2
    
    * Andreeva-2nd hw
    
    * 2 MagicNumber & Antipodes
    
    * Kolesnikov Practice #2
    
    * Andreeva
    
    * Hometask for lection 2
    
    * Task2 MagicNumbs and Opposite
    
    * add first task of homework number 2
    
    * add second task of homework number 2
    
    * add task of homework number 1
    
    * Lection 3
    
    Co-authored-by: atarasov <atarasov@mera.ru>
    Co-authored-by: Diana Eskina <diana.s.eskina@yandex.ru>
    Co-authored-by: nkuzmichev <kuzhexyz@gmail.com>
    Co-authored-by: Andreeva <tandr@te.mera.ru>
    Co-authored-by: Dmitrii L <dmchel91@gmail.com>
    Co-authored-by: krostis <krostis77@mail.ru>
    Co-authored-by: gladorange <pazitiffchik@gmail.com>
    Co-authored-by: anna-kozinova <anna.zemtsova@orioninc.com>
    Co-authored-by: odyadk <oleg.dyadkin@mera.com>
    Co-authored-by: Victoriya <alvi813@yandex.ru>

---
+++ __LECTIONS/3/com/mera/lesson3/Constructors.java
@@ -0,0 +1,17 @@
+package com.mera.lesson3;
+
+public class Constructors {
+
+
+    public static void main(String[] args) {
+        Person p = new Person("Вася","Пупкин");
+        p.printHello();
+
+        Person p2 = new Person("Алексей");
+        p2.printHello();
+
+
+
+    }
+
+}
