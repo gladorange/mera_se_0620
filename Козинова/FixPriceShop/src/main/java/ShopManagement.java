e34cd2d107348703fe92378fcec508c36803e223
anna-kozinova <anna.zemtsova@orioninc.com>
2020-07-03 15:25:48 +0300

    HomeTask for Lection3 (FixPriceShop)

--- Козинова/FixPriceShop/src/main/java/ShopManagement.java
+++ Козинова/FixPriceShop/src/main/java/ShopManagement.java
@@ -1,38 +1,27 @@
 import java.util.Random;
 
 public class ShopManagement {
-
-    public static void main(String[] args) {
-
+    static void checkPriceRandomItem(FixPriceShop shop){
         Random rand = new Random();
         String itemToFound;
         int timeToCheck;
-        int maxItems = 20;
+        int maxItems = Goods.GOODS_LIST.length;
 
-        FixPriceShop shop1 = new FixPriceShop(Goods.getRandomGoods(rand.nextInt(maxItems)));
-        itemToFound = Goods.GOODS_LIST[rand.nextInt(maxItems)];
+        itemToFound =Goods.GOODS_LIST[rand.nextInt(maxItems)];
         System.out.println("В этом магазине есть следующие товары:");
-        shop1.printItems();
-        System.out.println("В этом магазине единая цена: " + shop1.getPrice());
-        timeToCheck = rand.nextInt(24);
-        System.out.println("Стоимость товара: " + itemToFound + " в " + timeToCheck + ": " + shop1.checkItemPrice(itemToFound,timeToCheck));
-
-        FixPriceShop shop2 = new FixPriceShop(Goods.getRandomGoods(rand.nextInt(maxItems)));
-        itemToFound = Goods.GOODS_LIST[rand.nextInt(maxItems)];
-        System.out.println("В этом магазине есть следующие товары:");
-        shop2.printItems();
-        System.out.println("В этом магазине единая цена: " + shop2.getPrice());
-        timeToCheck = rand.nextInt(24);
-        System.out.println("Стоимость товара: " + itemToFound + " в " + timeToCheck + ": " + shop2.checkItemPrice(itemToFound,timeToCheck));
-
-        FixPriceShop shop3 = new FixPriceShop(Goods.getRandomGoods(rand.nextInt(maxItems)));
-        itemToFound = Goods.GOODS_LIST[rand.nextInt(maxItems)];
-        System.out.println("В этом магазине есть следующие товары:");
-        shop3.printItems();
-        System.out.println("В этом магазине единая цена: " + shop3.getPrice());
-        timeToCheck = rand.nextInt(24);
-        System.out.println("Стоимость товара: " + itemToFound + " в " + timeToCheck + ": " + shop3.checkItemPrice(itemToFound,timeToCheck));
-
+        shop.printItems();
+        System.out.println("В этом магазине единая цена: "+shop.getPrice());
+        timeToCheck =rand.nextInt(24);
+        System.out.println("Стоимость товара: "+itemToFound +" в "+timeToCheck +": "+shop.checkItemPrice(itemToFound,timeToCheck));
+}
+    public static void main(String[] args) {
+        Random randVal = new Random();
+        FixPriceShop shop1 = new FixPriceShop(Goods.getRandomGoods(randVal.nextInt(Goods.GOODS_LIST.length)));
+        checkPriceRandomItem(shop1);
+        FixPriceShop shop2 = new FixPriceShop(Goods.getRandomGoods(randVal.nextInt(Goods.GOODS_LIST.length)));
+        checkPriceRandomItem(shop2);
+        FixPriceShop shop3 = new FixPriceShop(Goods.getRandomGoods(randVal.nextInt(Goods.GOODS_LIST.length)));
+        checkPriceRandomItem(shop3);
 
 
     }
