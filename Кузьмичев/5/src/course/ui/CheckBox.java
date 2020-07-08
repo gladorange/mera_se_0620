9bb64d46e677ca9b5ea0fbb93bfdcde9ac22baa9
nkuzmichev <kuzhexyz@gmail.com>
2020-07-08 22:18:41 +0300

    Task #5

---
+++ Кузьмичев/5/src/course/ui/CheckBox.java
@@ -0,0 +1,37 @@
+package course.ui;
+
+public class CheckBox extends Element implements Clickable {
+    private Boolean checked;
+
+    public static Boolean DEFAULT_CHECHED;
+
+    static {
+        CheckBox.DEFAULT_CHECHED = false;
+    }
+
+    public CheckBox() {
+        this(DEFAULT_CHECHED);
+    }
+
+    public CheckBox(Boolean checked) {
+        super();
+        this.checked = checked;
+    }
+
+    public Boolean getChecked() {
+        return this.checked;
+    }
+
+    public void setChecked(Boolean checked) {
+        this.checked = checked;
+    }
+
+    @Override
+    public void click() throws Element.ReadyOnlyException{
+        if (!getEnable()) {
+            throw new Element.ReadyOnlyException();
+        }
+
+        setChecked(!getChecked());
+    }
+}
