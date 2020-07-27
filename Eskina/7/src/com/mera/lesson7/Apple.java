92f7a66c0779ff7fb9c8d8a50e460bb0da7283f9
Diana Eskina <diana.s.eskina@yandex.ru>
2020-07-27 14:41:21 +0300

    Homework 7 code style changes

--- Eskina/7/src/com/mera/lesson7/Apple.java
+++ Eskina/7/src/com/mera/lesson7/Apple.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Apple extends FoodItem {
-    public static String[] greenNames = {"Семеринка", "Белый налив", "Гренни смит"};
-    public static String[] redNames = {"RED DELISHIES", "Глостер"};
-    public static String[] yellowNames = {"Антоновка", "Голден"};
+    public static String[] GREEN_SORTS = {"Семеринка", "Белый налив", "Гренни смит"};
+    public static String[] RED_SORTS = {"RED DELISHIES", "Глостер"};
+    public static String[] YELLOW_SORTS = {"Антоновка", "Голден"};
 
     protected ColorEnum color;
 
@@ -54,9 +54,9 @@ public class Apple extends FoodItem {
         ColorEnum colorType = ColorEnum.generateRandomColor();
 
         switch (colorType) {
-            case RED -> name = redNames[rand.nextInt(redNames.length)];
-            case GREEN -> name = greenNames[rand.nextInt(greenNames.length)];
-            case YELLOW -> name = yellowNames[rand.nextInt(yellowNames.length)];
+            case RED -> name = RED_SORTS[rand.nextInt(RED_SORTS.length)];
+            case GREEN -> name = GREEN_SORTS[rand.nextInt(GREEN_SORTS.length)];
+            case YELLOW -> name = YELLOW_SORTS[rand.nextInt(YELLOW_SORTS.length)];
         }
         return new Apple(name,rand.nextInt(50, 100), rand.nextInt(40, 80), 30, colorType);
     }
